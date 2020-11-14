62168c0383814c5923f68a0da38300b6f54ff240
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2020-11-14 10:21:16 -0800

    Mais Aulas
    Operadores de Comparacao e Condicionais

---
+++ Arquivos/11_if/js/scripts.js
@@ -0,0 +1,22 @@
+var idade=18;
+var idadeAceite=20;
+
+var nome='Manuel';
+
+if (idade>=idadeAceite){
+    console.log('Pode entrar na Festa');
+}else{
+    console.log('Nao pode entrar na Festa');
+}
+
+if ( idade >= 18 ){
+    console.log('Maior de idade');
+}else{
+    console.log('Menor de idade');
+}
+
+if (nome=='Mateus'){
+    console.log('Nome dele e Mateus');
+}else if(nome !='Mateus'){
+    console.log('Nome nao e Mateus')
+}
---
+++ Arquivos/12_=== e !==/js/scripts.js
@@ -0,0 +1,16 @@
+var idade="12";
+
+if (idade === 12){
+    console.log('Adolescente');
+}else{
+    console.log('Crianca');
+}
+
+
+if (idade !== 12){
+    console.log('Passou')
+}else{
+    console.log('Perdeu')
+}
+
+/* Compara os numeros pelo valor e pelo tipo de dado*/
