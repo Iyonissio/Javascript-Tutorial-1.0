79e47eeaf826d082e2d2bf2ac4bcc6e5968b556b
Iyonissio <mbryoyo01@gmail.com>
2020-12-09 02:58:27 -0800

    new files

---
+++ 32_this/js/scripts.js
@@ -0,0 +1,12 @@
+console.log(this);
+
+alert('Ola');
+this.alert('Ola 2');
+
+let cao = {
+    nome: "Bobila",
+    idade: 2,
+    ladrar: function (){
+        console.log('Wow wow wow');
+    },
+};
