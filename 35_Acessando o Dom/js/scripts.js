ce43bb4a0ceed31dd4be57681d7bb594916a7dc8
Iyonissio <mbryoyo01@gmail.com>
2020-12-10 23:41:57 -0800

    Acessando elementos do Dom

---
+++ 35_Acessando o Dom/js/scripts.js
@@ -0,0 +1,17 @@
+var titulo = document.getElementsByTagName('h1')[0];
+
+console.log(titulo);
+
+var lista = document.getElementsByTagName('li');
+
+console.log(lista);
+
+var paragrafo = document.getElementById('paragrafo');
+
+console.log(paragrafo);
+
+
+//class
+
+var listaclasse = document.getElementsByClassName('item');
+console.log(listaclasse);
