bc05ac58ad581c6cce175d50ca88c1a06cd94651
Iyonissio <mbryoyo01@gmail.com>
2020-12-11 00:17:48 -0800

    Alterando elementos texto atraves do Dom no HTML

---
+++ 36_querySelector/js/scripts.js
@@ -0,0 +1,34 @@
+
+var listaquery1 = document.querySelectorAll('#lista li');
+console.log(listaquery1);
+
+var listaquery2 = document.querySelectorAll('#lista2 li');
+
+console.log(listaquery2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 37_Alterando Texto/js/scripts.js
@@ -0,0 +1,11 @@
+
+var novotitulo = document.querySelector('#titulo');
+
+//Metodo innerHTML para actualizar Texto
+novotitulo.innerHTML='Actualizei Titulo pelo Hinner';
+
+//metodo textConten
+
+ var novotitulo2 = document.querySelector('#titulo2');
+
+ novotitulo2.textContent = "Texto 2 ";
