59c2cd1d01c5fb05126245e3148883c170a07662
Iyonissio <mbryoyo01@gmail.com>
2020-12-12 02:16:24 -0800

    update 12/12/2020

---
+++ 40_Inserindo elemntos com DOM/js/scripts.js
@@ -0,0 +1,12 @@
+
+
+var elemento = document.createElement('div');
+
+elemento.classList = "div-criada";
+
+console.log(elemento);
+
+var container = document.querySelector('#container');
+
+container.appendChild(elemento);
+
